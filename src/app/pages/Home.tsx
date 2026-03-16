3ced52ddb09c09063c455d238602bd47a6e2baa9
JenKaiWang <158121249+JenKaiWang@users.noreply.github.com>
2026-03-16 10:34:02 -0500

    Refactor Home component structure

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -7,15 +7,13 @@ import { Footer } from "../components/Footer";
 
 export default function Home() {
   return (
-    <div className="bg-gradient-to-br from-indigo-950 via-purple-950 to-pink-950 min-h-screen text-slate-100 font-sans selection:bg-pink-500/30">
+    <>
       <Navbar />
-      <main>
-        <Hero />
-        <Experience />
-        <Volunteering />
-        <Projects />
-      </main>
+      <Hero />
+      <Experience />
+      <Volunteering />
+      <Projects />
       <Footer />
-    </div>
+    </>
   );
 }
